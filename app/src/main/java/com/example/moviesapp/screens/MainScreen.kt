af6056b24ff9bd360405fdd64b249f0bdac99dfd
andrey <andrey.ostapovich.90@mail.ru>
2024-03-24 21:28:30 +0300

    Lesson-1

---
+++ app/src/main/java/com/example/moviesapp/screens/MainScreen.kt
@@ -0,0 +1,24 @@
+package com.example.moviesapp.screens
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun MainScreen() {
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Text(text = "Test")
+    }
+}
+
+@Composable
+@Preview
+fun Main() {
+    MainScreen()
+}
